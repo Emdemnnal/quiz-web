5ce3f642e50673591a13bd9bdf8d4911f0009b0d
Crisolle Infante <crisolle.infante@gmail.com>
2022-09-21 11:20:39 +0100

    changed back-end URL

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,8 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    //service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'http://quizapici-env.eba-warmfypq.us-west-2.elasticbeanstalk.com/'
 
     return service;
 }]);
